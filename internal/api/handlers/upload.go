4a800bc26c3b0ff9e1cab4a84e0e8cd608d913ac
sickodev <kbishwa03kalyan@gmail.com>
2025-05-18 01:03:52 +0530

    adds upload method

--- internal/api/handlers/upload.go
+++ internal/api/handlers/upload.go
@@ -1,13 +1,44 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-func UploadHandler(c *gin.Context){
+func UploadHandler(c *gin.Context) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Missing Excel file",
+		})
+		return
+	}
+
+	ext := strings.ToLower(filepath.Ext(file.Filename))
+	if ext != ".xlsx" && ext != ".xls" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Only .xlsx or .xls file",
+		})
+		return
+	}
+
+	timestamp := time.Now().Unix()
+	savePath := fmt.Sprintf("uploads/%d_%s", timestamp, file.Filename)
+
+	if err := c.SaveUploadedFile(file, savePath); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to save file",
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"message":"Upload Endpoint hit",
+		"message":  "File uploaded successfully",
+		"filepath": savePath,
 	})
 }
